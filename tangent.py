f7ab310fcc1ddae768213d81b0198ea4d8c0898d
prjasti <prjasti@ctemc.org>
2018-01-10 13:56:01 -0500

    Update tangent.py

--- tangent.py
+++ tangent.py
@@ -1,16 +1,28 @@
 import numpy as np  
-import matplotlib.pyplot as pylab
-import sympy as sp
-x = np.linspace(-15,15,100) # 100 linearly spaced numbers
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+x = np.linspace(-15,15,200)
 def f(x):
-    return x ** 2
-y = f(x) # computing the values of sin(x)/x
-i = int(raw_input())
-dy = (f(i + 0.001) - f(i))/(0.001) * (x - i) + (f(i))
-l = (f(i + 2) - f(i))/2 * (x - i) + (f(i))
-j = (f(i + 5) - f(i))/5 * (x - i) + (f(i))
-pylab.plot(x,y) # sin(x)/x
-pylab.plot(x,dy)
-pylab.plot(x,l)
-pylab.plot(x,j)
-pylab.show()
+    return x ** 3
+y = f(x)
+def draw_tangent(i,dx):
+    dy = (f(i + 0.001) - f(i))/(0.001) * (x - i) + (f(i))
+    dx = (f(i + float(dx)) - f(i))/(float(dx)) * (x - i) + (f(i))
+    plt.plot(x,y,'black')
+    plt.plot(x,dy,'chartreuse')
+    plt.plot(x,dx,'r')
+    plt.show()
+def shade(n,i):
+    plt.plot(x,y,'black')
+    currentAxis = plt.gca()
+    orig_point = (0,0)
+    for a0 in range(n+1):
+        if a0 == n:
+            break
+        else:
+            currentAxis.add_patch(Rectangle(orig_point, orig_point[0] + i/float(n), f(orig_point[0] + i/float(n)), facecolor="blue"))
+            orig_point = ((a0+1) * i/float(n),0)
+    plt.axhline(0, color='black')
+    plt.axvline(0, color='black')
+    plt.show()
+
