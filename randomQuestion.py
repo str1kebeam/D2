ffe07aed6dbcde8025811d703f18aeccd9b18d23
TinyRex42 <elithorpe42@gmail.com>
2018-01-17 13:41:43 -0500

    Fixes generating 0x statements

--- randomQuestion.py
+++ randomQuestion.py
@@ -1,11 +1,13 @@
 import answerChecker as ac #hopefully this is't shorthand for something else
 import random
-'''Errors to fix:
-I can't have 0x ever show up, because that will break other stuff'''
 def askAQuestion(question, answer, simplify=True):
     ans=raw_input(question+" ")
     return ac.printCorrectNicely(ac.checkAnswer(ans, answer, simplify))
-
+def randomIntNo0(low, high):
+    a=random.randint(low, high)
+    while a==0:
+        a=random.randint(low,high)
+    return a
 def randomAddition(difficulty=1):
     '''Test of making random problems. Gives an addition/subtraction problem. Difficulty 0 corresponds to whole numbers <100,
     1 is integers -100<=x<=100, add more difficulties later.'''
@@ -35,24 +37,24 @@ def randomTangent(difficulty=1):
     1 gives a quadratic equation ax^2 + bx + c, where all of those variables are in the range (-10, 10)
     2 gives a polynomial equation ax^b + cx^d + e, where b and d are (1, 4) and not the same, and the rest are in the range (-10, 10)'''
     if difficulty==0:
-        a = random.randint(-10, 10)
+        a = randomIntNo0(-10, 10)
         b = random.randint(-10, 10)
         x = random.randint(-10, 10)
         answer=a
         question="What is the slope of the tangent line of y=%sx + %s at x=%s?"%(a,b,x)
         return question, answer, False
     elif difficulty==1:
-        a = random.randint(-10, 10)
-        b = random.randint(-10, 10)
+        a = randomIntNo0(-10, 10)
+        b = randomIntNo0(-10, 10)
         c = random.randint(-10, 10)
         x = random.randint(-10, 10)
         answer = (a*x*2)+b 
         question = "What is the slope of the tangent line of y = %sx^2 + %sx + %s at x=%s?"%(a, b, c, x)
         return question, answer, False
     elif difficulty==2:
-        a = random.randint(-10, 10)
+        a = randomIntNo0(-10, 10)
         b = random.randint(1, 4) #exponent here
-        c = random.randint(-10, 10)
+        c = randomIntNo0(-10, 10)
         d = random.randint(1, 4)
         while d==b:
             d=random.randint(1, 4)
@@ -72,22 +74,22 @@ def randomDerivative(difficulty=1):
     '''Just an example of the sympy answer checker parts, because none of the other questions use it and that's what I did a lot on
     The same as randomTangent, except it doesn't have an x=something'''
     if difficulty==0:
-        a=random.randint(-10,10)
+        a=randomIntNo0(-10,10)
         b=random.randint(-10,10)
         answer=str(a)#needs to be a string for my function to turn it into sympy
         question = "What is the derivative of %sx + %s?"%(a, b)
         return question, answer, False
     elif difficulty ==1:
-        a=random.randint(-10, 10)
-        b=random.randint(-10, 10)
+        a=randomIntNo0(-10, 10)
+        b=randomIntNo0(-10, 10)
         c=random.randint(-10, 10)
         answer=str(2*a)+"x"+str(b)
         question="What is the derivative of %sx^2 + %sx + %s?"%(a, b, c)
         return question, answer, False
     elif difficulty ==2:
-        a=random.randint(-10, 10)
+        a=randomIntNo0(-10, 10)
         b=random.randint(1, 4)
-        c=random.randint(-10, 10)
+        c=randomIntNo0(-10, 10)
         d=random.randint(1,4)
         while d==b:
             d=random.randint(1,4)
@@ -114,7 +116,7 @@ def randomDerivative(difficulty=1):
         else:
             answer+=str(c*d)+"x^"+str(d-1)
             question+=str(c)+"x^"+str(d)
-        question+="?"
+        question+=" + "+str(e)+"?"
         return question, answer, False
         
 problemTypes = {"addition":randomAddition, "tangent line":randomTangent, "derivative":randomDerivative}
