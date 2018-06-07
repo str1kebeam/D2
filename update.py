d130bf2b51a0979c2bf010bda0a065bca979b3ae
Eli Thorpe <elithorpe42@gmail.com>
2018-06-07 15:42:03 -0400

    Removes some old files
    Yeah, I'm not including 7 files in the list that just say 'no longer in use'

--- update.py
+++
@@ -1,8 +0,0 @@
-import os
-for filename in os.listdir(directory):
-    if filename.endswith(".html"):
-        with f as open(os.path.join(directory, filename), 'r'):
-            for l in f.readlines():
-        continue
-    else:
-        continue
